a1d654c07e10da858a20ee8438d7bb2a68db0df4
James Stanley <james@incoherency.co.uk>
2025-03-05 20:19:31 +0000

    Add radius option to box

--- js/nodes/primitives.js
+++ js/nodes/primitives.js
@@ -26,13 +26,14 @@ class SphereNode extends TreeNode {
 }
 
 class BoxNode extends TreeNode {
-  constructor(size = [1, 1, 1]) {
+  constructor(size = [1, 1, 1], radius = 0.0) {
     super("Box");
     this.size = size;
+    this.radius = radius;
   }
 
   properties() {
-    return {"size": "vec3"};
+    return {"size": "vec3", "radius": "float"};
   }
 
   shaderImplementation() {
@@ -41,11 +42,19 @@ class BoxNode extends TreeNode {
         vec3 d = abs(p) - b;
         return length(max(d, 0.0)) + min(max(d.x, max(d.y, d.z)), 0.0);
       }
+      float sdRoundBox(vec3 p, vec3 b, float r) {
+        vec3 q = abs(p) - b + r;
+        return length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0) - r;
+      }
     `;
   }
 
   generateShaderCode() {
-    return `sdBox(p, vec3(${this.size[0].toFixed(16)}, ${this.size[1].toFixed(16)}, ${this.size[2].toFixed(16)}))`;
+    if (this.radius > 0.0) {
+      return `sdRoundBox(p, vec3(${this.size[0].toFixed(16)}, ${this.size[1].toFixed(16)}, ${this.size[2].toFixed(16)}), ${this.radius.toFixed(16)})`;
+    } else {
+      return `sdBox(p, vec3(${this.size[0].toFixed(16)}, ${this.size[1].toFixed(16)}, ${this.size[2].toFixed(16)}))`;
+    }
   }
 
   getIcon() {
