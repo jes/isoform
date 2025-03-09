23392cc716d75236be2e91638ba2b2da83d9f136
James Stanley <james@incoherency.co.uk>
2025-03-09 11:55:47 +0000

    Make torus face Z axis

--- js/nodes/primitives.js
+++ js/nodes/primitives.js
@@ -131,7 +131,7 @@ class TorusNode extends TreeNode {
   generateShaderImplementation() {
     return `
       float sdTorus(vec3 p, vec2 t) {
-        vec2 q = vec2(length(p.xz) - t.x, p.y);
+        vec2 q = vec2(length(p.xy) - t.x, p.z);
         return length(q) - t.y;
       }
     `;
